8815420f8c6f350e80aa532927dd43f90cefab5f
Jefferson Phillips <jeffersongphillips@gmail.com>
2024-02-11 15:41:33 -0500

    Refactor command calls

--- zfs-pool-stats.py
+++ zfs-pool-stats.py
@@ -18,11 +18,12 @@ POOL_NAME = "amalgm"
 REPEAT_DELAY = "1.0"
 
 
-def shelllllllllllllllllllllllll(cmdline):
+# TODO: Rewrite this function to run commands locally, once this script is complete.
+def shell(cmdline):
     """Run shell commands via SSH and return output (stdout or stderr).
     The only argument accepted is cmdline, the command to run."""
     result = subprocess.run(["ssh", "root@192.168.1.33", cmdline],
-                            capture_output=True, text=True, check=False)
+                            capture_output=True, text=True, check=True)
 
     if result.returncode == 0:
         return result.stdout
@@ -30,57 +31,43 @@ def shelllllllllllllllllllllllll(cmdline):
         return result.stderr
 
 
-# TODO: Refactor all these into one master set of keys, which retreives values and appends as we go
-### Ingest data from `iostat` ###
-iostat_keys = ["pName", "pCapLogicUsed", "pCapLogicFree", "pOpsRead", "pOpsWrite", "pBwRead", "pBwWrite",
-               "pTotalwaitRead", "pTotalwaitWrite", "pDiskwaitRead", "pDiskwaitWrite", "pSyncqwaitRead",
-               "pSyncqwaitWrite", "pAsyncqwaitRead", "pAsyncqwaitWrite", "pScrubWait", "pTrimWait"]
-#iostat_vals = shell("zpool iostat -Hypl " + POOL_NAME + " " + REPEAT_DELAY + " " + "1").split()
-iostat_vals = ["amalgm", "51567724367872", "16344298516480", "16", "0", "8468325", "0", "15682379", "-", "15682379", "-", "3532", "-", "3510", "-", "-", "-"]
-iostat = dict(zip(iostat_keys, iostat_vals))  # Zip together `iostat` headers and values to create a dictionary
+### Ingest data from `iostat`, `zfs get`, `zpool status` ###
 
-# print(iostat)
-# for c, v in iostat.items():
-#      print(c, v)
+# Starting from ["pName"], the values of `zpool iostat` will be assigned
+# Starting from ["pCapVirtUsed"], the values of `zfs get` will be assigned
+# Starting from ["pStateHealth"], the values of `zfs get` (again) will be assigned
+# Starting from ["pStateText"], the values of `zpool status` will be assigned
 
-### Ingest data from `zfs get` ###
-zfsget_keys = ["pCapVirtUsed", "pCapVirtFree", "pCapCompPerc", "pCapUsedBychilds", "pCapUsedBysnapshots"]
-#zfsget_vals = shell("zfs get used,available,compressratio,usedbychildren " + POOL_NAME +
-#                    " -Hp -d 0 -o value | tr '\n' ' '").split()
-zfsget_vals = ["54866186481664", "12908397449216", "1.01", "54700434006016"]
+# In case the output sequence from any of these underlying commands ever changes in a future version,
+# this dictionary will become misaligned, and require a re-evaluation of each underlying command.
 
+stats_keys = ["pName", "pCapLogicUsed", "pCapLogicFree", "pOpsRead", "pOpsWrite", "pBwRead", "pBwWrite",
+              "pTotalwaitRead", "pTotalwaitWrite", "pDiskwaitRead", "pDiskwaitWrite", "pSyncqwaitRead",
+              "pSyncqwaitWrite", "pAsyncqwaitRead", "pAsyncqwaitWrite", "pScrubWait", "pTrimWait",
+              "pCapVirtUsed", "pCapVirtFree", "pCapCompPerc", "pCapUsedBychilds", "pCapUsedBysnapshots",
+              "pStateHealth", "pStateFrag", "pStateText"]
+
+stats_vals = []
+stats_vals.extend(["amalgm", "51567724367872", "16344298516480", "16", "0", "8468325",
+                   "0", "15682379", "-", "15682379", "-", "3532", "-", "3510", "-", "-", "-"])  # shell("zpool iostat -Hypl " + POOL_NAME + " " + REPEAT_DELAY + " " + "1").split()
+# shell("zfs get used,available,compressratio,usedbychildren " + POOL_NAME + " -Hp -d 0 -o value | tr '\n' ' '").split()
+stats_vals.extend(["54866186481664", "12908397449216", "1.01", "54700434006016"])
 # TODO: This `zfs get usedbysnapshots` command is very slow, because it's recursively checking
-#       all snapshots sizes (-r) and summing them (awk) before returning. Both operations are intensive.
+#       all snapshots sizes (`-r`) and summing them (`awk`) before returning.
 #       Also, this method with `grep` and `awk` is very clunky and may break with future `zfs` versions.
+#       Parsing and addition should be performed locally.
+# shell("zfs get usedbysnapshots " + POOL_NAME + " -Hp -r -o value | grep -v '-' | awk '{s+=$1} END {printf \"%.0f\", s}'").split())
+stats_vals.extend(["1381425606656"])
+stats_vals.extend(["ONLINE", "20%"])  # shell("zpool list -H -o health,frag " + POOL_NAME)
+# TODO: Clean up this `zpool status` command and perform text parsing locally instead.
+# shell("zpool status " + POOL_NAME + " | sed -n '3,$p' | tr '\n' ' ' | tr -d '\011\012' | sed -e 's/^[ \t]*//' | " + "sed --regexp-extended 's/ config\:.*//g'")
+stats_vals.extend(
+    ["scan: scrub repaired 0B in 1 days 12:59:37 with 0 errors on Sat Jan 27 22:59:39 2024 remove: Removal of mirror canceled on Tue Jan  9 08:30:58 2024"])
 
-#zfsget_vals.extend(shell("zfs get usedbysnapshots " + POOL_NAME +
-#                         " -Hp -r -o value | grep -v '-' | awk '{s+=$1} END {printf \"%.0f\", s}'").split())
-zfsget_vals.extend(["1381425606656"])
+print("statskeys\n", stats_keys)
+print("statsvals\n", stats_vals)
 
-zfsget = dict(zip(zfsget_keys, zfsget_vals))
+stats = dict(zip(stats_keys, stats_vals))
 
-# Ingest data from `zpool list`
-zpoollist_keys = ["pStateHealth", "pStateFrag"]
-#zpoollist_vals = shell("zpool list -H -o health,frag " + POOL_NAME)
-zpoollist_vals = ["ONLINE", "20%"]
-
-zpoollist = dict(zip(zfsget_keys, zfsget_vals))
-
-# Ingest data from `zpool status`
-zpoolstatus_keys = ["pStateDescript"]
-# TODO: Clean up this bash mess and parse the values locally
-#zpoolstatus_vals = shell("zpool status " + POOL_NAME +
-#                         " | sed -n '3,$p' | tr '\n' ' ' | tr -d '\011\012' | sed -e 's/^[ \t]*//' | " +
-#                         "sed --regexp-extended 's/ config\:.*//g'")
-zpoolstatus_vals = ["scan: scrub repaired 0B in 1 days 12:59:37 with 0 errors on Sat Jan 27 22:59:39 2024 remove: Removal of mirror canceled on Tue Jan  9 08:30:58 2024"]
-
-zpoolstatus = dict(zip(zfsget_keys, zfsget_vals))
-
-# print("iostat", iostat)
-# print("zfsget", zfsget)
-# print("zpoollist", zpoollist)
-# print("zpoolstatus", zpoolstatus)
-
-for list in iostat, zfsget, zpoollist, zpoolstatus:
-    for key, value in list.items():
-        print(key, value)
+for key, value in stats.items():
+    print(f"{key} : {value}")
