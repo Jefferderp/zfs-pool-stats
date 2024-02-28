aa2dcbd275f0e93c156341aedabef0ed41914c9a
Jefferson Phillips <jeffersongphillips@gmail.com>
2024-02-28 16:02:18 -0500

    Make args.POOL required

--- zfs-pool-stats.py
+++ zfs-pool-stats.py
@@ -8,34 +8,66 @@ import random
 
 
 """ TODO:
-* When an invalid column is passed to --columns, exit with an error instead of ignoring silently.
+* Restore shell_cmd() lines commented out and remove placeholder lists
+
+* Change shell_cmd() to run locally instead of remotely
+
+* Write a basic document of how data flows from start to finish.
+    * From which arguments
+    * From which system commands
+    * Through which functions, to which functions
+    * At what stage each conv_X() function is called, and what it does
+    
+    * ...to have mercy on my future self and others.
+    * I tried to avoid the "nesting china doll" approach, and with the exception of implementing curses,
+      mostly succeedded in keeping everything separate and modular. However, many functions are not
+      input-agnostic and will error if input is not speciall formatted.
+      That's fine though, I wrote a script, not a framework.
+
+* When an invalid column is passed to --columns, exit with an error instead of continuing silently.
+
+* An -c+ flag to specify additional columns to append to the default set, rather than re-specifying the whole set.
+
 * Add a third sub-argument to --columns for decimal places
+
 * Add a fourth sub-argument to --columns for custom column names printed at runtime. Also include
   default column names which are more sane. Or maybe scrap this idea and use sane column names to begin with.
-* Set default script parameters if no flags passed
+
 * Shorten length of key names in {zpool_keys} in preparation for column output, where width is at a premium
-* Strip whitespace from args.COLUMNS to prevent dictionary key mis-matches, causing failure to print.
-* Standardize all input flags to lowercase handling
-* Repeated output in aligned columns with automatic minimum width
-* Upgrade conv_microseconds() to round up by length (99s = 1.65m != 100s), instead of time (59s > 1m)
-Add to conv_bytes():
-* Up/down/nearest rounding preference per-invocation or per-value
-* A sticky header with ["stateHealth"] and ["stateText"]
+
+* Strip whitespace from args.COLUMNS input, to prevent dictionary key mis-matches.
+
+* Standardize all input flags to lowercase handling, to prevent mismatches
+
+* Improve conv_microseconds() and conv_bytes() to round up by length (99s = 1.65m != 100s), instead of time (59s > 1m)
+
+* Add a sticky header with ["stateHealth"] and ["stateText"]
+
 * Use first line of `zpool iostat` (without -y) to get statistics since boot, and display as the last line (sticky)
-   and with some special stylization (bold, underlined, etc.)
-* An -c+ flag to add additional columns to the default set (a'la `lsblk`)
+  and with some special stylization (bold, underlined, etc.)
+
 * Implement text coloring for pool state health
-* A brief warning if the user specifies a REPEAT_DELAY < 1
-* Restore shell_cmd() lines commented out and remove placeholder lists
-* Change shell_cmd() to run locally instead of remotely
-* Try removing the need for math module
+
+* A brief warning if the user specifies a REPEAT_DELAY < 1, that stats will be less precise.
+
+* Try removing the need for modules: math, random
+
 * Implement threading or async so that any delay inherent to get_stats() isn't added to --interval
-* Implement multiple simultaneous pool outputs
-* Add internal ingestion delays of between 5 and 15 seconds (to save CPU) for any relatively stable values,
-      which are extremely unlikely to change +/- 1%. These are:
-      Name, LogicCapUsed, LogicCapFree, VirtCapUsed, VirtCapFree, VirtCompRatio,
+
+* Implement multiple simultaneous pool outputs (this would take a gigantic effort)
+
+* Add internal delays in get_stats() of between 5 and 15 seconds (to save CPU) for any relatively stable values,
+which are extremely unlikely to change +/- 1%. These are:
+    * Name, LogicCapUsed, LogicCapFree, VirtCapUsed, VirtCapFree, VirtCompRatio,
       VirtCapUsedByChilds, VirtCapUsedBySnaps, StateHealth, StateFragPerc, StateText
+    * Use time module to either calculate or pass based on the current second of the current minute.
+
+* Experiment with curses in print_columns() to remove the weird header and scrolling behavior on init
 """
+
+#####  Accept arguments  #####
+
+# Define arguments parser
 
 
 def parse_complex_arg(string):
@@ -67,11 +99,10 @@ def parse_complex_arg(string):
         raise argparse.ArgumentTypeError("ERROR: Invalid format for --columns. Use Column1,Column2, ... ")
 
 
-###  Accept arguments  ###
-# Define arguments parser
+# Assign arguments parser
 parser = argparse.ArgumentParser()
 
-# Construct args.COLUMNS dictionary
+# Construct args.COLUMNS dictionary from --columns (-c) flag
 parser.add_argument('--columns', '-c', dest="COLUMNS", type=parse_complex_arg,
                     # Default columns if none specified:
                     # TODO: VirtCapUsedPerc,VirtCompPerc are being ignored?
@@ -79,23 +110,23 @@ parser.add_argument('--columns', '-c', dest="COLUMNS", type=parse_complex_arg,
                     help='A comma-separated list of columns to output. Optionally specify :scale. \
                           For example:  --columns PoolName,StateHealth,VirtCapFree:T ')
 
-# Construct args.INTERVAL dictionary
+# Construct args.INTERVAL dictionary from --interval (-t) flag
 parser.add_argument('--interval', '-t', dest="INTERVAL", type=float,
                     default=1.0,  # Default delay interval of 1 second, if not specified.
                     help='The frequency of time (in seconds) to output statistics. Accepts whole or decimal numbers. \
                           This also affects the sampling of some delay measurements; the recommendation is 1 second \
-                          or more to allow a sufficient sampling window for collecting i/o timing statistics. \
+                          or more, to allow a sufficient sampling window for collecting i/o timing statistics. \
                           For example:  --interval 1.5 ')
 
-# Construct args.POOL dictionary
+# Construct args.POOL dictionary from --pool (-p) flag
 parser.add_argument('--pool', '-p', dest="POOL", type=str,
-                    default="amalgm",  # TODO: Retrieve default args.POOL name intelligently from the system...somehow.
+                    required=True,
                     help='The name of the pool to report statistics for. For example:  --pool tank ')
 
 args = parser.parse_args()  # Expose args.COLUMNS, args.INTERVAL, etc. for use
 
 
-###  Define functions  ###
+#####  Define functions  #####
 
 
 def shell_cmd(cmdline):
@@ -111,121 +142,7 @@ def shell_cmd(cmdline):
     result = subprocess.run(["ssh", "root@192.168.1.33", cmdline],
                             capture_output=True, text=True, check=True)
 
-    # if result.returncode == 0:
-    #     return result.stdout
-    # else:
-    #     return result.stderr
     return result.stdout if result.returncode == 0 else result.stderr
-
-
-def conv_float(value):
-    """Try to convert string to float, otherwise pass through original input.
-
-    Args:
-        value: The value (string or int) to convert to float.
-
-    Returns:
-        The float representation of value.
-    """
-    try:
-        value = value.strip('-%')  # Remove undesired chars
-        return float(value) if value else 0  # Convert eligible strings to floats. Convert empty strings to 0.
-    except ValueError:  # If failed to convert to float, return as original type.
-        return value
-
-
-def conv_bytes(bytes, notation=None):
-    """Convert byte values to a specified notation. Uses powers of 1024 as output by `zfs get`.
-
-    Args:
-        bytes: The byte value (int or float) to convert.
-        notation: The desired notation ('B', 'K', 'M', 'G', 'T', 'P', 'E').
-            If unspecified, automatically chooses the highest notation.
-
-    Returns:
-        A string representing the byte value expressed in the chosen notation."""
-    # Handle 0 and strings. Return them unmodified.
-    if bytes == 0 or isinstance(bytes, str):
-        return bytes
-
-    notations = ("B", "K", "M", "G", "T", "P", "E")
-
-    if notation is None:  # Automatic unit scaling, if not specified.
-        i = int(math.floor(math.log(bytes, 1024)))  # Math, how does it work?!
-        p = math.pow(1024, i)
-        return f"{round(bytes / p)}{notations[i]}"
-
-    try:  # Manual unit scaling, if specified.
-        notation = str(notation[0])
-        index = notations.index(notation.upper())  # Find index of target notation
-        divisor = 1024 ** index  # Calculate byte value to divide by
-        return f"{round(bytes / divisor)}{notation}"
-    except ValueError:
-        print(f"ValueError: {notation} is not one of: {notations}")
-
-
-def conv_str(input, notation=None):
-    """Accepts any valid input and returns a string.
-    This function is used instead of the built-in str() because str()
-    will error when receiving an invalid second parameter.
-
-    Args:
-        input: The input value to convert to a string.
-        notation: Does nothing. Exists for compatibility reasons.
-
-    Returns:
-        input formatted as a string.
-    """
-
-    return str(input)
-
-
-def conv_perc(input, notation=None):
-    """Accepts any valid input and returns a percentage value as a string.
-
-    Args:
-        input: The input value to convert to a string.
-        notation: Does nothing. Exists for compatibility reasons.
-
-    Returns:
-        input formatted as a string, with a '%' appended.
-    """
-
-    # return str(f"{(input)}%")
-    # return round(input, 2)
-    # Return a string with no leading/trailing decimals, and append a '%'
-    return f"{input:.0%}"
-
-
-def conv_microseconds(microseconds, notation=None):
-    """Convert microsecond values to a specified notation. Uses microseconds to align with `zpool iostat -p`.
-
-    Args:
-        microseconds: The microsecond value (int or float) to convert.
-        notation: The desired notation ('d', 'h', 'm', 's', 'ms', 'us').
-            If unspecified, automatically chooses the highest notation.
-
-    Returns:
-        A string representing the time value expressed in the chosen notation."""
-    # Handle 0 and strings. Return them unmodified.
-    if microseconds == 0 or isinstance(microseconds, str):
-        return microseconds
-
-    notations = {"d": 86400000000, "h": 3600000000, "m": 60000000, "s": 1000000, "ms": 1000, "us": 1}
-
-    if notation is None:  # Automatic unit scaling, if not specified.
-        for i, key in enumerate(notations):
-            if microseconds >= (notations[key] - 0.0001):  # Subtract a small rounding tolerance
-                divisor = notations[key]
-                notation = key
-                break  # Exit the loop once the appropriate unit is found
-        return f"{round(microseconds / divisor)}{notation}"
-
-    try:  # Manual unit scaling, if specified.
-        notation = str(notation[0])
-        return f"{round(microseconds / notations[notation])}{notation}"
-    except KeyError:
-        print(f"ValueError: {notation} is not one of: {notations}")
 
 
 def get_stats(pool):
@@ -291,6 +208,116 @@ def get_stats(pool):
     return zpool
 
 
+def conv_float(value):
+    """Try to convert string to float, otherwise pass through original input.
+
+    Args:
+        value: The value (string or int) to convert to float.
+
+    Returns:
+        The float representation of value.
+    """
+    try:
+        value = value.strip('-%')  # Remove undesired chars
+        return float(value) if value else 0  # Convert eligible strings to floats. Convert empty strings to 0.
+    except ValueError:  # If failed to convert to float, return as original type.
+        return value
+
+
+def conv_str(input, notation=None):
+    """Accepts any valid input and returns a string.
+    This function is used instead of the built-in str() because str()
+    will error when receiving an invalid second parameter.
+
+    Args:
+        input: The input value to convert to a string.
+        notation: Does nothing. Exists for compatibility reasons.
+
+    Returns:
+        input formatted as a string.
+    """
+
+    return str(input)
+
+
+def conv_perc(input, notation=None):
+    """Accepts any valid input and returns a percentage value as a string.
+
+    Args:
+        input: The input value to convert to a string.
+        notation: Does nothing. Exists for compatibility reasons.
+
+    Returns:
+        input formatted as a string, with a '%' appended.
+    """
+
+    # Return a string with no leading/trailing decimals, and append a '%'
+    return f"{input:.0%}"
+
+
+def conv_bytes(bytes, notation=None):
+    """Convert byte values to a specified notation. Uses powers of 1024 as output by `zfs get`.
+
+    Args:
+        bytes: The byte value (int or float) to convert.
+        notation: The desired notation ('B', 'K', 'M', 'G', 'T', 'P', 'E').
+            If unspecified, automatically chooses the highest notation.
+
+    Returns:
+        A string representing the byte value expressed in the chosen notation."""
+
+    # Handle 0 and strings. Return them unmodified.
+    if bytes == 0 or isinstance(bytes, str):
+        return bytes
+
+    notations = ("B", "K", "M", "G", "T", "P", "E")
+
+    if notation is None:  # Automatic unit scaling, if not specified.
+        i = int(math.floor(math.log(bytes, 1024)))  # Math, how does it work?!
+        p = math.pow(1024, i)
+        return f"{round(bytes / p)}{notations[i]}"
+
+    try:  # Manual unit scaling, if specified.
+        notation = str(notation[0])
+        index = notations.index(notation.upper())  # Find index of target notation
+        divisor = 1024 ** index  # Calculate byte value to divide by
+        return f"{round(bytes / divisor)}{notation}"
+    except ValueError:
+        print(f"ValueError: {notation} is not one of: {notations}")
+
+
+def conv_microseconds(microseconds, notation=None):
+    """Convert microsecond values to a specified notation. Uses microseconds to align with `zpool iostat -p`.
+
+    Args:
+        microseconds: The microsecond value (int or float) to convert.
+        notation: The desired notation ('d', 'h', 'm', 's', 'ms', 'us').
+            If unspecified, automatically chooses the highest notation.
+
+    Returns:
+        A string representing the time value expressed in the chosen notation."""
+
+    # Handle 0 and strings. Return them unmodified.
+    if microseconds == 0 or isinstance(microseconds, str):
+        return microseconds
+
+    notations = {"d": 86400000000, "h": 3600000000, "m": 60000000, "s": 1000000, "ms": 1000, "us": 1}
+
+    if notation is None:  # Automatic unit scaling, if not specified.
+        for i, key in enumerate(notations):
+            if microseconds >= (notations[key] - 0.0001):  # Subtract a small rounding tolerance
+                divisor = notations[key]
+                notation = key
+                break  # Exit the loop once the appropriate unit is found
+        return f"{round(microseconds / divisor)}{notation}"
+
+    try:  # Manual unit scaling, if specified.
+        notation = str(notation[0])
+        return f"{round(microseconds / notations[notation])}{notation}"
+    except KeyError:
+        print(f"ValueError: {notation} is not one of: {notations}")
+
+
 def conv_dict_notation(ref_keys, conv_keys):
     """Convert the values in a dictionary from raw integer/time values to human-readable notation.
 
@@ -307,22 +334,21 @@ def conv_dict_notation(ref_keys, conv_keys):
 
     output = {}
 
-    for key, notation in conv_keys.items():
+    for key_name, notation in conv_keys.items():
         # Try all possible permutations of key_match in {ref_keys}.
-        #   NOTE: This is because each key of {ref_keys} is a tuple of (key_match, key_type) but
-        #   we want to access the key by just key_match, hence this crutch. Maybe we could
+        #   NOTE: This is because each key of {ref_keys} is a tuple of (key_name, key_type) but
+        #   we want to access the key by just key_name, hence this crutch. Maybe we could
         #   specify key_type in zpool_vals instead of zpool_keys, however this may be impractical.
         for key_type in zpool_keys_types:  # Try all key_type to match against keys in {ref_keys}
-            key_match = (key, key_type)  # Construct a tuple to properly match key_match against {ref_keys}
-            if key_match in ref_keys:  # Proceed once the correct match for key_match has been found
-                # Check which function to use for conversion
-                # zpool_keys_map is defined in get_stats()
+            key_match = (key_name, key_type)  # Construct a tuple to properly match key_match against {ref_keys}
+            if key_match in ref_keys:  # Proceed once the correct match for key_match has been found in {key_refs}
+                # Check which function to use for conversion. {zpool_keys_map} is a global defined in get_stats()
                 key_use_func = zpool_keys_map.get(key_match[1])
 
                 # Calculate notation and append the key from {conv_keys} to {output}:
-                output.update({key: key_use_func(ref_keys[key_match], notation)})
+                output.update({key_name: key_use_func(ref_keys[key_match], notation)})
 
-                # Print everything for debugging
+                # Print for debugging. TODO: Remove this
                 # print(f"{key} : {notation[0]} : {ref_keys[key_match]} : {key_use_func} : {key_use_func(ref_keys[key_match])}")
                 # print(f"ref_keys: {ref_keys}")
 
@@ -349,7 +375,7 @@ def get_keys_width(input_dict):
     return column_widths
 
 
-def print_columns(input_dict=conv_dict_notation(get_stats(args.POOL), args.COLUMNS), interval=args.INTERVAL):
+def print_columns(input_dict=conv_dict_notation(get_stats(args.POOL), args.COLUMNS), interval=max(args.INTERVAL, 0.01)):
     """On a loop, print out a dictionary in columns format.
 
     Args:
@@ -392,8 +418,8 @@ def print_columns(input_dict=conv_dict_notation(get_stats(args.POOL), args.COLUM
                 scr_row = scr_height - 1
 
             stdscr.addstr(1, 0, header + "\n")  # Repeatedly print columns header on first line
-            # TODO: Remove debug random.randint() value
-            stdscr.addstr(scr_row, 0, f"{values} {random.randint(100, 999)} \n")  # Repeatedly print latest values
+            # TODO: Remove random.randint() value used for debugging
+            stdscr.addstr(scr_row, 0, f"{values} {random.randint(90, 110)} \n")  # Repeatedly print latest values
             stdscr.refresh()
 
             time.sleep(interval)
@@ -402,13 +428,9 @@ def print_columns(input_dict=conv_dict_notation(get_stats(args.POOL), args.COLUM
     curses.wrapper(stdscr, header, values)
 
 
-###  Run output  ###
+#####  Print output  #####
+
 try:
-    # raw_stats = get_stats(args.POOL)
-    # converted_keys = convert_keys(ref_keys=raw_stats, conv_keys=args.COLUMNS)
-    # print_columns(converted_keys, args.INTERVAL)
-
     print_columns()
-
 except KeyboardInterrupt:  # Exit gracefully on ^C (SIGINT)
     exit
